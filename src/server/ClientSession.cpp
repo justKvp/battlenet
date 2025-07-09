c973e17274db41c4fecb7c32db685fe98a78aeec
Владимиров Александр <kvipka90@yandex.ru>
2025-07-09 19:42:17 +0300

    upd ClientSession

--- src/server/ClientSession.cpp
+++ src/server/ClientSession.cpp
@@ -1,53 +1,58 @@
 #include "ClientSession.hpp"
-#include "Server.hpp"
 #include <iostream>
 
 using boost::asio::ip::tcp;
 
-ClientSession::ClientSession(tcp::socket socket_, Server &server_)
-        : socket(std::move(socket_)), server(server_) {}
+ClientSession::ClientSession(tcp::socket socket)
+        : socket_(std::move(socket)) {}
 
 void ClientSession::start() {
     read_header();
 }
 
 void ClientSession::read_header() {
+    header_buffer_.resize(2);
     auto self = shared_from_this();
-    read_buffer.resize(2); // 2 байта заголовка
-
-    boost::asio::async_read(socket, boost::asio::buffer(read_buffer),
+    boost::asio::async_read(socket_, boost::asio::buffer(header_buffer_),
                             [this, self](boost::system::error_code ec, std::size_t /*length*/) {
                                 if (ec) {
-                                    std::cerr << "[Server] Header read failed: " << ec.message() << "\n";
-                                    server.remove_session(self);
+                                    if (ec == boost::asio::error::eof) {
+                                        std::cout << "[Server] Client closed the connection\n";
+                                    } else {
+                                        std::cerr << "[Server] Header read failed: " << ec.message() << "\n";
+                                    }
+                                    socket_.close();
                                     return;
                                 }
 
-                                uint16_t size = (read_buffer[0] << 8) | read_buffer[1];
+                                uint16_t size = (header_buffer_[0] << 8) | header_buffer_[1];
                                 read_body(size);
                             });
 }
 
-void ClientSession::read_body(std::size_t length) {
+void ClientSession::read_body(std::size_t size) {
+    body_buffer_.resize(size);
     auto self = shared_from_this();
-    read_buffer.resize(length);
-
-    boost::asio::async_read(socket, boost::asio::buffer(read_buffer),
+    boost::asio::async_read(socket_, boost::asio::buffer(body_buffer_),
                             [this, self](boost::system::error_code ec, std::size_t /*length*/) {
                                 if (ec) {
-                                    std::cerr << "[Server] Body read failed: " << ec.message() << "\n";
-                                    server.remove_session(self);
+                                    if (ec == boost::asio::error::eof) {
+                                        std::cout << "[Server] Client closed the connection\n";
+                                    } else {
+                                        std::cerr << "[Server] Body read failed: " << ec.message() << "\n";
+                                    }
+                                    socket_.close();
                                     return;
                                 }
 
                                 try {
-                                    Packet packet = Packet::deserialize(read_buffer);
+                                    Packet packet = Packet::deserialize(body_buffer_);
                                     handle_packet(packet);
                                 } catch (...) {
-                                    std::cerr << "[Server] Failed to parse packet\n";
+                                    std::cerr << "[Server] Failed to deserialize packet\n";
                                 }
 
-                                read_header(); // Continue reading next packet
+                                read_header();
                             });
 }
 
@@ -62,7 +67,7 @@ void ClientSession::handle_packet(Packet &packet) {
         }
         case Opcode::MESSAGE: {
             std::string msg = packet.buffer.read_string();
-            std::cout << "[Server] Received message: " << msg << "\n";
+            std::cout << "[Server] Received Message: " << msg << "\n";
             break;
         }
         default:
@@ -72,14 +77,13 @@ void ClientSession::handle_packet(Packet &packet) {
 }
 
 void ClientSession::send_packet(const Packet &packet) {
-    auto data = std::make_shared<std::vector<uint8_t>>(packet.serialize());
-
+    std::vector<uint8_t> data = packet.serialize();
     auto self = shared_from_this();
-    boost::asio::async_write(socket, boost::asio::buffer(*data),
-                             [this, self, data](boost::system::error_code ec, std::size_t /*length*/) {
+    boost::asio::async_write(socket_, boost::asio::buffer(data),
+                             [this, self](boost::system::error_code ec, std::size_t /*length*/) {
                                  if (ec) {
-                                     std::cerr << "[Server] Failed to send packet: " << ec.message() << "\n";
-                                     server.remove_session(self);
+                                     std::cerr << "[Server] Write failed: " << ec.message() << "\n";
+                                     socket_.close();
                                  }
                              });
 }
--- src/server/ClientSession.hpp
+++ src/server/ClientSession.hpp
@@ -2,27 +2,24 @@
 
 #include <boost/asio.hpp>
 #include <memory>
-#include <vector>
 #include "Packet.hpp"
-
-class Server;
 
 class ClientSession : public std::enable_shared_from_this<ClientSession> {
 public:
-    ClientSession(boost::asio::ip::tcp::socket socket, Server &server);
+    ClientSession(boost::asio::ip::tcp::socket socket);
 
     void start();
-
-    void send_packet(const Packet &packet);
 
 private:
     void read_header();
 
-    void read_body(std::size_t length);
+    void read_body(std::size_t size);
 
     void handle_packet(Packet &packet);
 
-    boost::asio::ip::tcp::socket socket;
-    Server &server;
-    std::vector<uint8_t> read_buffer;
+    void send_packet(const Packet &packet);
+
+    boost::asio::ip::tcp::socket socket_;
+    std::vector<uint8_t> header_buffer_;
+    std::vector<uint8_t> body_buffer_;
 };
