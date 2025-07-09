83f4568f074fce4777347107b2e6dbd6f9bfa01a
Владимиров Александр <kvipka90@yandex.ru>
2025-07-09 19:56:04 +0300

    typo

--- src/server/main_server.cpp
+++ src/server/main_server.cpp
@@ -6,7 +6,6 @@ int main() {
     try {
         boost::asio::io_context io_context;
         Server server(io_context, 12345);
-        server.start_accept();
         io_context.run();
     } catch (const std::exception &e) {
         std::cerr << "[Server] Exception: " << e.what() << std::endl;
