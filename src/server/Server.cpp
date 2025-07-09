cb8bb524feced018a5de048cce9a1864af9a0f1b
Владимиров Александр <kvipka90@yandex.ru>
2025-07-09 19:12:10 +0300

    fixed3

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -1,48 +1,114 @@
 #include "Server.hpp"
-#include "ClientSession.hpp"
-#include <thread>
+#include <iostream>
 
 using boost::asio::ip::tcp;
 
-Server::Server(boost::asio::io_context &io, short port)
-        : acceptor(io, tcp::endpoint(tcp::v4(), port)) {
-    do_accept();
+Session::Session(tcp::socket socket_, Server& server_)
+        : socket(std::move(socket_)), server(server_) {}
 
-    std::thread([this]() {
-        while (true) {
-            {
-                std::lock_guard<std::mutex> lock(session_mutex);
-                auto now = std::chrono::steady_clock::now();
-                for (auto it = sessions.begin(); it != sessions.end();) {
-                    auto session = *it;
-                    session->check_timeout(now);
-                    if (!session->socket.is_open()) {
-                        it = sessions.erase(it);
-                    } else {
-                        ++it;
-                    }
-                }
-            }
-            std::this_thread::sleep_for(std::chrono::seconds(5));
-        }
-    }).detach();
+void Session::start() {
+    read_header();
 }
 
-void Server::do_accept() {
-    acceptor.async_accept([this](boost::system::error_code ec, tcp::socket socket) {
-        if (!ec) {
-            auto session = std::make_shared<ClientSession>(std::move(socket), *this);
-            {
-                std::lock_guard<std::mutex> lock(session_mutex);
-                sessions.insert(session);
-            }
-            session->start();
-        }
-        do_accept();
-    });
+void Session::read_header() {
+    auto self(shared_from_this());
+    read_buffer.resize(2);
+    boost::asio::async_read(socket, boost::asio::buffer(read_buffer),
+                            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                                if (!ec) {
+                                    uint16_t size = (read_buffer[0] << 8) | read_buffer[1];
+                                    read_body(size);
+                                } else {
+                                    std::cerr << "[Server] Read header error: " << ec.message() << "\n";
+                                    server.remove_session(self);
+                                }
+                            });
 }
 
-void Server::remove_session(std::shared_ptr<ClientSession> session) {
-    std::lock_guard<std::mutex> lock(session_mutex);
+void Session::read_body(std::size_t length) {
+    auto self(shared_from_this());
+    read_buffer.resize(length);
+    boost::asio::async_read(socket, boost::asio::buffer(read_buffer),
+                            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                                if (!ec) {
+                                    try {
+                                        Packet p = Packet::deserialize(read_buffer);
+                                        handle_packet(p);
+                                    } catch (...) {
+                                        std::cerr << "[Server] Failed to parse packet\n";
+                                    }
+                                    read_header();
+                                } else {
+                                    std::cerr << "[Server] Read body error: " << ec.message() << "\n";
+                                    server.remove_session(self);
+                                }
+                            });
+}
+
+void Session::handle_packet(Packet& packet) {
+    switch (packet.opcode) {
+        case Opcode::PING: {
+            std::cout << "[Server] Received PING\n";
+            Packet pong;
+            pong.opcode = Opcode::PONG;
+            send_packet(pong);
+            break;
+        }
+        case Opcode::MESSAGE: {
+            std::string msg = packet.buffer.read_string();
+            std::cout << "[Server] Message: " << msg << "\n";
+            break;
+        }
+        default:
+            std::cout << "[Server] Unknown opcode: " << static_cast<int>(packet.opcode) << "\n";
+            break;
+    }
+}
+
+void Session::send_packet(const Packet& packet) {
+    auto self(shared_from_this());
+    std::vector<uint8_t> data = packet.serialize();
+    write_queue.push_back(data);
+
+    if (write_queue.size() == 1) {
+        do_write();
+    }
+}
+
+void Session::do_write() {
+    auto self(shared_from_this());
+    boost::asio::async_write(socket, boost::asio::buffer(write_queue.front()),
+                             [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                                 if (!ec) {
+                                     write_queue.pop_front();
+                                     if (!write_queue.empty()) {
+                                         do_write();
+                                     }
+                                 } else {
+                                     std::cerr << "[Server] Write error: " << ec.message() << "\n";
+                                     server.remove_session(self);
+                                 }
+                             });
+}
+
+Server::Server(boost::asio::io_context& io_context, int port)
+        : acceptor(io_context, tcp::endpoint(tcp::v4(), port)) {
+    start_accept();
+}
+
+void Server::start_accept() {
+    auto socket = std::make_shared<tcp::socket>(acceptor.get_executor());
+    acceptor.async_accept(*socket,
+                          [this, socket](boost::system::error_code ec) {
+                              if (!ec) {
+                                  auto session = std::make_shared<Session>(std::move(*socket), *this);
+                                  sessions.insert(session);
+                                  session->start();
+                              }
+                              start_accept();
+                          });
+}
+
+void Server::remove_session(std::shared_ptr<Session> session) {
     sessions.erase(session);
 }
--- src/server/Server.hpp
+++ src/server/Server.hpp
@@ -1,21 +1,40 @@
 #pragma once
-#include <boost/asio.hpp>
-#include <set>
-#include <memory>
-#include <mutex>
 
-class ClientSession;
+#include <boost/asio.hpp>
+#include <memory>
+#include <set>
+#include <deque>
+#include "Packet.hpp"
+
+class Server;
+
+class Session : public std::enable_shared_from_this<Session> {
+public:
+    Session(boost::asio::ip::tcp::socket socket, Server& server);
+    void start();
+    void send_packet(const Packet& packet);
+
+private:
+    void read_header();
+    void read_body(std::size_t length);
+    void handle_packet(Packet& packet);
+    void do_write();
+
+    boost::asio::ip::tcp::socket socket;
+    std::vector<uint8_t> read_buffer;
+    std::deque<std::vector<uint8_t>> write_queue;
+    Server& server;
+};
 
 class Server {
 public:
-    Server(boost::asio::io_context& io, short port);
-    void remove_session(std::shared_ptr<ClientSession> session);
+    Server(boost::asio::io_context& io_context, int port);
+    void start_accept();
+    void remove_session(std::shared_ptr<Session> session);
 
 private:
-    void do_accept();
-    void monitor_clients();
+    void handle_accept(std::shared_ptr<Session> session, const boost::system::error_code& error);
 
     boost::asio::ip::tcp::acceptor acceptor;
-    std::set<std::shared_ptr<ClientSession>> sessions;
-    std::mutex session_mutex;
+    std::set<std::shared_ptr<Session>> sessions;
 };
